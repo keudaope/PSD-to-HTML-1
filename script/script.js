66ec9fd1845fda07de5f680c2b24eb092a678f8e
Jyri Lindroos <59079476+keudaope@users.noreply.github.com>
2024-01-24 06:25:20 +0200

    JQUERY

---
+++ script/script.js
@@ -0,0 +1,72 @@
+// Tehtävä 5.
+//$("p").css("color", "red");
+
+// Tehtävä 6
+//$("img").hide();
+
+// Tehtävä 7
+//$("header div").hide("slow");
+
+//Tehtävä 8
+//$("li").addClass("selected");
+
+//Tehtävä9
+//$("img").removeAttr("alt");
+
+// Tehtävä 10
+//$("img").width("50%");
+
+// Tehtävä 11
+//$(".banner-title").text("Osaamme Web-suunnittelun");
+
+// Tehtävä 12
+//$("#contact-methods").append("<li>About us</li>");
+
+// Tehtävä 13
+//var firstH3 = $("h3:first").clone();
+//$("body").append(firstH3);
+
+// Tehtävä 14
+//$("h3").hover(function(){
+//    $(this).css("color", "blue");
+//});
+
+// Tehtävä 15
+//$("a").attr("target", "_blank");
+
+// Tehtävä 16
+//$("section h2").click(function(){
+//    $("nav").toggle();
+//});
+
+// Tehtävä 17
+//$("h1, h2, h3, h4, h5, h6").wrapInner("<span> Otsikko: </span>");
+
+// Tehtävä 18
+//$("a").css("font-size", "+=2px");
+
+// Tehtävä 19
+//$("header li:first").click(function(){
+//    alert("Home clicked!");
+//});
+
+// Tehtävä 20
+//$("header li:last").addClass("active");
+
+// Tehtävä 21
+//$("li").mouseover(function(){
+//    $(this).children("p").css("color", "green");
+//});
+
+// Tehtävä 22
+//$(document).ready(function(){
+//    alert("Document ready!");
+//});
+
+// Tehtävä 23
+//$("div").hover(function(){
+//  $(this).animate({fontSize: "24px"}, "slow");
+//});
+
+// Tehtävä 24
+$("img").css("border", "3px solid red").css("border-style", "dashed");
